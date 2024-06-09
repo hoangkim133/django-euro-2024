81e4ad950e19fc6c0d30755b57767b97a306bb1e
phamkimhoang <phamkimhoang133@gmail.com>
2024-06-09 22:55:45 +0700

    update key

--- api/settings.py
+++ api/settings.py
@@ -19,12 +19,10 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-print(os.environ)
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
