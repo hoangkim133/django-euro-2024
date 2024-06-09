1f51f129ce5ff0aad8d2d792f775e34ebf847ad1
phamkimhoang <phamkimhoang133@gmail.com>
2024-06-09 23:20:18 +0700

    key

--- api/settings.py
+++ api/settings.py
@@ -12,15 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 
-print(os.environ)
-print("----------")
-if os.environ.get('SECRET_KEY') is None:
-    load_dotenv()
-
-print(os.environ)
-print("----------")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
