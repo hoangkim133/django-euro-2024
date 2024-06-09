da91a1dc9cb2ca847c5a0fa65342b7bc45cb70b2
phamkimhoang <phamkimhoang133@gmail.com>
2024-06-09 23:01:25 +0700

    key

--- api/settings.py
+++ api/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()
+if os.environ.get('VERCEL') is None:
+    load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
