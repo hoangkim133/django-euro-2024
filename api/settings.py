1bd7bd85325dc292ab9e56e028cf1de5e8a03710
phamkimhoang <phamkimhoang133@gmail.com>
2024-06-20 19:38:28 +0700

    update

--- api/settings.py
+++ api/settings.py
@@ -121,9 +121,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'expenseswebsite', 'static'),
+)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'staticfiles_build', 'static'),
 # )
--- api/urls.py
+++ api/urls.py
@@ -24,4 +24,3 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
