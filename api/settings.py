9db6b6e5e53339f7b84d41acd3f54233a2d73dc1
phamkimhoang <phamkimhoang133@gmail.com>
2024-06-20 19:22:38 +0700

    udpate

--- api/settings.py
+++ api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -121,9 +121,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'staticfiles_build', 'static'),
+# )
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- euro/urls.py
+++ euro/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.getGroup),
@@ -11,5 +9,3 @@ urlpatterns = [
     path('teams', views.getTeam),
     path('stats', views.getStats),
 ]
-
-print(urlpatterns)
