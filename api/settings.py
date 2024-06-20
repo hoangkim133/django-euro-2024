c6be104048a93d7788156287d70fc81f30e3bc08
phamkimhoang <phamkimhoang133@gmail.com>
2024-06-20 19:44:17 +0700

    update

--- api/settings.py
+++ api/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'staticfiles_build', 'static'),
 # )
