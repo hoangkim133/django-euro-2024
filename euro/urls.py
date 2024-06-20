35ba0bf4fcd414b76f37ce8309f4a41ee8b58b4d
phamkimhoang <phamkimhoang133@gmail.com>
2024-06-20 18:00:17 +0700

    update

--- euro/urls.py
+++ euro/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.getGroup),
@@ -8,4 +10,4 @@ urlpatterns = [
     path('braket', views.getBraketview),
     path('teams', views.getTeam),
     path('stats', views.getStats),
-]
+] + static(settings.STATIC_ROOT, document_root=settings.STATIC_ROOT)
